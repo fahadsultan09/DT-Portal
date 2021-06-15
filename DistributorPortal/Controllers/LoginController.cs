a9f06308b8d0d28027fd972cdc31e80ab9b01792
Ehtasham Ahmed Khan <ehtasham.ahmed@samipharma.com.pk>
2021-06-15 15:17:22 +0500

    Save Changes

--- DistributorPortal/Controllers/LoginController.cs
+++ DistributorPortal/Controllers/LoginController.cs
@@ -174,17 +174,26 @@ namespace DistributorPortal.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Logout()
         {
-            new AuditLogBLL(_unitOfWork).AddAuditLog("Login", "Logout", "Start Click on Logout Button of ");
-            HttpContext.Session.Clear();
-            SessionHelper.LoginUser = null;
-            SessionHelper.NavigationMenu = null;
-            SessionHelper.AddProduct = null;
-            SessionHelper.AddDistributorWiseProduct = null;
-            SessionHelper.AddReturnProduct = null;
-            SessionHelper.DistributorBalance = null;
-            SessionHelper.SAPOrderPendingQuantity = null;
-            SessionHelper.SAPOrderPendingValue = null;
-            return RedirectToAction("Index", "Login");
+            if (SessionHelper.LoginUser.IsDistributor)
+            {
+                HttpContext.Session.Clear();
+                return View();
+            }
+            else
+            {
+                new AuditLogBLL(_unitOfWork).AddAuditLog("Login", "Logout", "Start Click on Logout Button of ");
+                HttpContext.Session.Clear();
+                SessionHelper.LoginUser = null;
+                SessionHelper.NavigationMenu = null;
+                SessionHelper.AddProduct = null;
+                SessionHelper.AddDistributorWiseProduct = null;
+                SessionHelper.AddReturnProduct = null;
+                SessionHelper.DistributorBalance = null;
+                SessionHelper.SAPOrderPendingQuantity = null;
+                SessionHelper.SAPOrderPendingValue = null;
+                return RedirectToAction("Index", "Login");
+            }
+            
         }
         [DllImport("Iphlpapi.dll")]
         private static extern int SendARP(int dest, int host, ref long mac, ref int length);
